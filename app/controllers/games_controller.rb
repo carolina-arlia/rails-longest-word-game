2f2ab63e2b502480eee6ac18eac11efc3a5c6566
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-16 01:39:51 +0200

    private methods in controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,15 +5,35 @@ class GamesController < ApplicationController
 
   def new
     @letters = []
-    @letters << ("A".."Z").to_a.sample until @letters.size == 10
+    @letters << ("A".."Z").to_a.sample(10)
   end
 
   def score
-    @letters
-    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
-    my_data_url = URI.open(url).read
-    my_data_url_into_hash = JSON.parse(my_data_url)
-    @response = my_data_url_into_hash["found"]
+    @word = params[:word].upcase
+    @word_array = params[:word].upcase.split("")
+    @letters = params[:letters].split
+    @included = included?(@word_array, @letters)
+    @english_word = english_word?(@word)
   end
 
+  private
+
+  def included?(word_array, letters)
+    letters_matched = []
+
+    word_array.each do |letter|
+      if letters.include?(letter)
+        letters_matched << letter
+        letters.delete_at(letters.index(letter))
+      end
+    end
+    letters_matched.size == word_array.size
+  end
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    my_data_url = URI.open(url).read
+    data_json = JSON.parse(my_data_url)
+    data_json["found"]
+  end
 end
